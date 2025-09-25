e4d220475038eab3c6f0626cc389bd6db9b05cd4
Robert Shaw <robshaw@redhat.com>
2025-09-25 08:46:00 -0700

    updated
    Signed-off-by: Robert Shaw <robshaw@redhat.com>

--- README.md
+++ README.md
@@ -6,14 +6,7 @@ We install the user space packages to local user without requiring use of sudo. 
 
 #### User Space
 - source the envs.sh (or add to your bashrc)
-- install gdrcopy (note: this just installs the user space stuff. We still need to install the kernel driver if we want to actually use GDRCopy)
 - install nvshmem
 - build deepep
 - build deepgemm
 
-#### Kernel Space
-- TODO
-
-#### Notes
-- pplx uses GDRCopy for multi-node, so we will need the driver for this
-- deepep uses IGBDA for multi-node. so we will need to ensure this is enabled 
--- install_nvshmem.sh
+++ install_nvshmem.sh
@@ -6,6 +6,24 @@ export CUDA_MAJOR=12
 export CUDA_ARCH_LIST="90a;100"
 export BUILD_VENV=nvshmem_build
 
+# disable all features except IBGDA
+export NVSHMEM_IBGDA_SUPPORT=1
+
+export NVSHMEM_SHMEM_SUPPORT=0
+export NVSHMEM_UCX_SUPPORT=0
+export NVSHMEM_USE_NCCL=0
+export NVSHMEM_PMIX_SUPPORT=0
+export NVSHMEM_TIMEOUT_DEVICE_POLLING=0
+export NVSHMEM_USE_GDRCOPY=0
+export NVSHMEM_IBRC_SUPPORT=0
+export NVSHMEM_BUILD_TESTS=0
+export NVSHMEM_BUILD_EXAMPLES=0
+export NVSHMEM_MPI_SUPPORT=0
+export NVSHMEM_BUILD_HYDRA_LAUNCHER=0
+export NVSHMEM_BUILD_TXZ_PACKAGE=0
+export NVSHMEM_TIMEOUT_DEVICE_POLLING=0
+
+
 wget https://developer.download.nvidia.com/compute/redist/nvshmem/${NVSHMEM_VERSION}/source/nvshmem_src_cuda12-all-all-${NVSHMEM_VERSION}.tar.gz -O nvshmem_src_cuda${CUDA_MAJOR}.tar.gz && \
     tar -xf nvshmem_src_cuda${CUDA_MAJOR}.tar.gz && \
     cd nvshmem_src && \
@@ -19,18 +37,6 @@ wget https://developer.download.nvidia.com/compute/redist/nvshmem/${NVSHMEM_VERS
         -G Ninja \
         -DNVSHMEM_PREFIX=${NVSHMEM_DIR} \
         -DCMAKE_CUDA_ARCHITECTURES=${CUDA_ARCH_LIST} \
-        -DNVSHMEM_PMIX_SUPPORT=0 \
-        -DNVSHMEM_LIBFABRIC_SUPPORT=0 \
-        -DNVSHMEM_IBRC_SUPPORT=1 \
-        -DNVSHMEM_IBGDA_SUPPORT=1 \
-        -DNVSHMEM_IBDEVX_SUPPORT=1 \
-        -DNVSHMEM_SHMEM_SUPPORT=0 \
-        -DNVSHMEM_USE_GDRCOPY=1 \
-        -DNVSHMEM_MPI_SUPPORT=0 \
-        -DNVSHMEM_USE_NCCL=0 \
-        -DNVSHMEM_BUILD_TESTS=0 \
-        -DNVSHMEM_BUILD_EXAMPLES=0 \
-        -DGDRCOPY_HOME=${GDRCOPY_HOME} \
         .. && \
         ninja -j$(nproc) && \
         ninja install && \
